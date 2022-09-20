dee30623cc2cc059e971e1d24dd76a3e582e49ae
haukatkinson <haukatkinson@aol.com>
2022-09-20 22:18:55 +0000

    navbar & jumbotron started

--- src/front/js/component/jumbotron.js
+++ src/front/js/component/jumbotron.js
@@ -1,21 +1,18 @@
 import React, { useEffect, useContext } from "react";
+// import jumboImg from "./jumboImg";
 
 const Jumbotron = () => {
-    const { store, actions } = useContext(Context);
-  
-    return (
-      <div
-        className="p-5 text-center bg-image rounded-3"
-        style={{
-          backgroundImage:
-            "url('https://mdbcdn.b-cdn.net/img/new/slides/041.webp')",
-          backgroundSize: "cover",
-          height: "250px",
-          width: "100%",
-        }}
-      >
-      </div>
-    );
-  };
-  export { Jumbotron };
-  
+  return (
+    <div
+      className="p-5 text-center bg-image rounded-3"
+      style={{
+        backgroundImage:
+          "url('https://mdbcdn.b-cdn.net/img/new/slides/041.webp')",
+        backgroundSize: "cover",
+        height: "250px",
+        width: "100%",
+      }}
+    ></div>
+  );
+};
+export { Jumbotron };
